576fb45a1dc5268dfb933fee467c2263831c40ff
Esraa Al-Hamrawy <91664628+EsraaHamrawy@users.noreply.github.com>
2026-08-13 13:25:39 +0300

    Clean up imports in home.page.jsx
    Removed unused imports and commented-out code in home.page.jsx.

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -51,37 +51,8 @@ const Home = () => {
             </div>
             <div className={`${style.fadeInUp} ${style.delay600}`}>
                <BackToTopButton />
-              <p className={style.smallResume}>
-                import { Grid } from "@mui/material";
-import { motion } from "framer-motion";
+              {/* <p className={style.smallResume}> */}
 
-
-import Experiences from "../Experiences/Experiences.jsx"
-import ContactMe from "../Contact/Contact.jsx";
-import Skills from "../Skills/Skills.jsx";
-import ChromaGrid from "../Certifications/Certifications.jsx"
-import cv  from "../../assets/Esraa Alhamrawy Frontend Developer.pdf"
-import { slideIn } from "../../utils/motion.js";
-import style from "./Hero.module.css";
-import { styles } from "../../styles";
-import Illustration from "../../assets/Programming(3).svg";
-import Skils from "../../assets/Skills .svg";
-import MyWork from "../../assets/plennixLogo.svg";
-import Hello from "../../assets/Hello.gif";
-import Socialmedia from "../../components/layout/socialmedia/socialmedia.component.jsx";
-import Card from "../../components/layout/socialmedia/newSocial.jsx";
-import MagicBento from "../../components/general/MagicBento/MagicBento.jsx";
-import Plasma from "../../components/general/Plasma/Plasma.jsx";
-import DynamicButton from "../../components/general/dynamicButton/DynamicButton.jsx";
-import Html from "../../assets/icons8-html-5-48.png";
-import Css from "../../assets/icons8-css-48.png";
-import Js from "../../assets/icons8-js-48.png";
-import React from "../../assets/icons8-react-48.png";
-import bootstrap from "../../assets/icons8-bootstrap-48.png";
-import Redux from "../../assets/icons8-redux-50.png";
-import github from "../../assets/icons8-github-logo-48.png";
-import {textVariant} from "../../utils/motion.js"
-import BackToTopButton from "../../components/general/backToTopButton/BackToTopButton.jsx";
 const Home = () => {
   const scrollToContact = () => {
     const section = document.getElementById('Contact');
